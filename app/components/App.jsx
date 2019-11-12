0e416428726159a0a612594131656f69c86c2582
Glitch (starter-react) <none>
2019-11-12 08:45:13 +0000

    🎬🍎 Checkpoint ./app/components/App.jsx:1703100/3654

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,61 +1,61 @@
-const React = require('react');
+const React = require("react");
 
 const firstName = {
-  "a": "Blubberbutt",
-  "b": "Benedict",
-  "c": "Benadryl",
-  "d": "Benchthis",
-  "e": "Bonapart",
-  "f": "Brokenbrick",
-  "g": "Boppinstick",
-  "h": "Benefit",
-  "i": "Scissorkick",
-  "j": "Backitup",
-  "k": "Beelzebub",
-  "l": "Burgerking",
-  "m": "Blenderdick",
-  "n": "Billiardball",
-  "o": "Guiltyverdict",
-  "p": "Beanbag",
-  "q": "Carrotstick",
-  "r": "Brodyquest",
-  "s": "Beachbody",
-  "t": "Bendylick",
-  "u": "Baseballmitt",
-  "v": "Bedbug",
-  "w": "Bunsenburner",
-  "x": "Bengaltiger",
-  "y": "Budapest",
-  "z": "Handpicked"
+  a: "Blubberbutt",
+  b: "Benedict",
+  c: "Benadryl",
+  d: "Benchthis",
+  e: "Bonapart",
+  f: "Brokenbrick",
+  g: "Boppinstick",
+  h: "Benefit",
+  i: "Scissorkick",
+  j: "Backitup",
+  k: "Beelzebub",
+  l: "Burgerking",
+  m: "Blenderdick",
+  n: "Billiardball",
+  o: "Guiltyverdict",
+  p: "Beanbag",
+  q: "Carrotstick",
+  r: "Brodyquest",
+  s: "Beachbody",
+  t: "Bendylick",
+  u: "Baseballmitt",
+  v: "Bedbug",
+  w: "Bunsenburner",
+  x: "Bengaltiger",
+  y: "Budapest",
+  z: "Handpicked"
 };
 
 const lastName = {
-  "a": "Calldispatch",
-  "b": "Comedicmismatch",
-  "c": "Cunningscratch",
-  "d": "Cumberfinch",
-  "e": "Humperdinck",
-  "f": "Lumberlatch",
-  "g": "Flubbercrack",
-  "h": "Cumberbatch",
-  "i": "Bandersnatch",
-  "j": "Cuttlefish",
-  "k": "Slumberbelch",
-  "l": "Cupboardlatch",
-  "m": "Combyourthatch",
-  "n": "Thundermunch",
-  "o": "Cricketbat",
-  "p": "Johnnycash",
-  "q": "Comelycat",
-  "r": "Custardbath",
-  "s": "Thundercats",
-  "t": "Numbercrunch",
-  "u": "Luckycatch",
-  "v": "Covertrack",
-  "w": "Uptoscratch",
-  "x": "Compasstrap",
-  "y": "Chunkybap",
-  "z": "Candygram"
+  a: "Calldispatch",
+  b: "Comedicmismatch",
+  c: "Cunningscratch",
+  d: "Cumberfinch",
+  e: "Humperdinck",
+  f: "Lumberlatch",
+  g: "Flubbercrack",
+  h: "Cumberbatch",
+  i: "Bandersnatch",
+  j: "Cuttlefish",
+  k: "Slumberbelch",
+  l: "Cupboardlatch",
+  m: "Combyourthatch",
+  n: "Thundermunch",
+  o: "Cricketbat",
+  p: "Johnnycash",
+  q: "Comelycat",
+  r: "Custardbath",
+  s: "Thundercats",
+  t: "Numbercrunch",
+  u: "Luckycatch",
+  v: "Covertrack",
+  w: "Uptoscratch",
+  x: "Compasstrap",
+  y: "Chunkybap",
+  z: "Candygram"
 };
 
 const firstNameArray = Object.values(firstName);
@@ -63,9 +63,12 @@ const lastNameArray = Object.values(lastName);
 
 const generatedName = function() {
   return {
-    name: firstNameArray[Math.floor(Math.random() * 26)] + " " + lastNameArray[Math.floor(Math.random() * 26)]
+    name:
+      firstNameArray[Math.floor(Math.random() * 26)] +
+      " " +
+      lastNameArray[Math.floor(Math.random() * 26)]
   };
-}
+};
 
 const App = class Name extends React.Component {
   constructor(props) {
@@ -73,19 +76,19 @@ const App = class Name extends React.Component {
     this.state = generatedName();
     this.handler = this.handler.bind(this);
   }
-  
+
   handler() {
     this.setState(generatedName());
   }
-  
+
   render() {
     return (
       <div>
         <h1>{this.state.name}</h1>
         <button onClick={this.handler}>Another one!</button>
       </div>
-    )
+    );
   }
-}
+};
 
 module.exports = App;
